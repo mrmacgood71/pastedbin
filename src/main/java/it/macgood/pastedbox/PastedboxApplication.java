2a32c51eaef3aa626a8ded7f737557de5278eb92
macgood <bo2002277@gmail.com>
2023-07-23 17:19:27 +0300

    Initial commit

---
+++ src/main/java/it/macgood/pastedbox/PastedboxApplication.java
@@ -0,0 +1,13 @@
+package it.macgood.pastedbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastedboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastedboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/macgood/pastedbox/PastedboxApplicationTests.java
@@ -0,0 +1,13 @@
+package it.macgood.pastedbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastedboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
